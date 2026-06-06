e3be01551e4d6129717edf9f8336fdde154dc910
Dashik94 <dashik4@gmail.com>
2026-06-06 21:51:37 +0300

    Add input Validation

--- calc.py
+++ calc.py
@@ -1,5 +1,9 @@
-num1 = float(input("Введите первое число: "))
-num2 = float(input("Введите второе число: "))
+try:
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+except ValueError:
+    print("Ошибка! Нужно вводить числа.")
+    exit()
 
 operation = input("Введите операцию (+ - * /): ")
 
