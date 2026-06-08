cd20a683ecb56548224b6ec70485985b5f65e815
Dashik94 <dashik4@gmail.com>
2026-06-08 21:22:25 +0300

    Цикличность

--- calc.py
+++ calc.py
@@ -1,26 +1,23 @@
-try:
-    num1 = float(input("Введите первое число: "))
-    num2 = float(input("Введите второе число: "))
-except ValueError:
-    print("Ошибка! Нужно вводить числа.")
-    exit()
-
-operation = input("Введите операцию (+ - * /): ")
-
-if operation == "+":
-    print("Результат:", num1 + num2)
-
-elif operation == "-":
-    print("Результат:", num1 - num2)
-
-elif operation == "*":
-    print("Результат:", num1 * num2)
-
-if operation == "/":
-    if num2 == 0:
-        print("Ошибка! На ноль делить нельзя.")
+continue_work = "Да"
+while continue_work == "Да":
+    try:
+        num1 = float(input("Введите первое число: "))
+        num2 = float(input("Введите второе число: "))
+    except ValueError:
+        print("Ошибка! Нужно вводить числа.")
+        exit()
+    operation = input("Введите операцию (+ - * /): ")
+    if operation == "+":
+     print("Результат:", num1 + num2)
+    elif operation == "-":
+        print("Результат:", num1 - num2)
+    elif operation == "*":
+        print("Результат:", num1 * num2)
     elif operation == "/":
-        print("Результат:", num1 / num2)
-
-else:
-    print("Неизвестная операция")
+        if num2 == 0:
+            print("Ошибка! На ноль делить нельзя.")
+        else:
+            print("Результат:", num1 / num2)
+    else:
+        print("Неизвестная операция")
+    continue_work = input("Продолжить работу? Да/Нет:")
