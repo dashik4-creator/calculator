a716788240c68c8c677a5fddbcf6a63b5207d477
Dashik94 <dashik4@gmail.com>
2026-06-10 23:06:45 +0300

    never-ending cycle

--- calc.py
+++ calc.py
@@ -1,46 +1,52 @@
-from math_functions import add, subtract, multiply, divide, show_menu
-continue_work = "Да"
+from math_functions import add, subtract, multiply, divide, show_menu, show_history, show_action_menu
+continue_work = "1"
 history = []
 print(history)
-while continue_work == "Да":
-    try:
-        num1 = float(input("Введите первое число: "))
-        num2 = float(input("Введите второе число: "))
-    except ValueError:
-        print("Ошибка! Нужно вводить числа.")
-        num1 = float(input("Введите первое число: "))
-        num2 = float(input("Введите второе число: "))
-    show_menu ()
-    operation = input("Введите операцию: ")
-    if operation == "1":
-        result = add(num1, num2)
-        history.append(f"{num1} + {num2} = {result}")
-        print("Результат:", result)
-    elif operation == "2":
-        result = subtract(num1, num2)
-        history.append(f"{num1} - {num2} = {result}")
-        print("Результат:", result)
-    elif operation == "3":
-        result = multiply(num1, num2)
-        history.append(f"{num1} * {num2} = {result}")
-        print("Результат:", result)
-    elif operation == "4":
-        if num2 == 0:
-            print("Ошибка! На ноль делить нельзя.")
-        else:
-            result = divide(num1, num2)
-            history.append(f"{num1} / {num2} = {result}")
-            print("Результат:", result)
+try:
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+except ValueError:
+    print("Ошибка! Нужно вводить числа.")
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+show_menu ()
+operation = input("Введите операцию: ")
+if operation == "1":
+    result = add(num1, num2)
+    history.append(f"{num1} + {num2} = {result}")
+    print("Результат:", result)
+elif operation == "2":
+    result = subtract(num1, num2)
+    history.append(f"{num1} - {num2} = {result}")
+    print("Результат:", result)
+elif operation == "3":
+    result = multiply(num1, num2)
+    history.append(f"{num1} * {num2} = {result}")
+    print("Результат:", result)
+elif operation == "4":
+    if num2 == 0:
+        print("Ошибка! На ноль делить нельзя.")
     else:
-        print("Неизвестная операция")
-    continue_work = input("Продолжить работу? Да/Нет:")
-    if continue_work == "Нет":
+        result = divide(num1, num2)
+        history.append(f"{num1} / {num2} = {result}")
+        print("Результат:", result)
+else:
+    print("Неизвестная операция")
+while True:
+    show_action_menu()
+    continue_work = input("Продолжить работу? (1-4): ")
+    if continue_work == "2":
+        show_history(history)
+    if continue_work == "3":
+        history.clear ()
+        print("История очищена!")
+    if continue_work == "4":
         print("Cпасибо за использование калькулятора!")
-        print("История операций:")
-        for entry in history:
-            print(entry)
-    if continue_work != "Да" and continue_work != "Нет":
-        print("Ошибка! Нужно вводить 'Да' или 'Нет'.")
-        continue_work = input("Продолжить работу? Да/Нет:")
-        if continue_work == "Нет":
-            print("Cпасибо за использование калькулятора!")
+        show_history(history)
+        break
+    if continue_work != "1" and continue_work != "2" and continue_work != "3" and continue_work != "4":
+        print("Ошибка! Нужно вводить '1', '2', '3' или '4'.")
+    
+        #continue_work = input("Продолжить работу? (1-4): ")
+        
+    
--- math_functions.py
+++ math_functions.py
@@ -11,3 +11,12 @@ def show_menu():
     print("2 - Вычитание")
     print("3 - Умножение")
     print("4 - Деление")
+def show_history(history):
+    print("История операций:")
+    for entry in history:
+        print(entry)
+def show_action_menu():
+    print("1 - Продолжить работу")
+    print("2 - Показать историю")
+    print("3 - Очистить историю")
+    print("4 - Выйти")
