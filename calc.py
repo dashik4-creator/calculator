16e1ed7842ca4fc4638492ed5addae6df3573ec2
Dashik94 <dashik4@gmail.com>
2026-06-10 14:56:56 +0300

    history

--- calc.py
+++ calc.py
@@ -1,5 +1,7 @@
 from math_functions import add, subtract, multiply, divide
 continue_work = "Да"
+history = []
+print(history)
 while continue_work == "Да":
     try:
         num1 = float(input("Введите первое число: "))
@@ -15,24 +17,31 @@ while continue_work == "Да":
     operation = input("Введите операцию: ")
     if operation == "1":
         result = add(num1, num2)
+        history.append(f"{num1} + {num2} = {result}")
         print("Результат:", result)
     elif operation == "2":
         result = subtract(num1, num2)
+        history.append(f"{num1} - {num2} = {result}")
         print("Результат:", result)
     elif operation == "3":
         result = multiply(num1, num2)
+        history.append(f"{num1} * {num2} = {result}")
         print("Результат:", result)
     elif operation == "4":
         if num2 == 0:
             print("Ошибка! На ноль делить нельзя.")
         else:
             result = divide(num1, num2)
+            history.append(f"{num1} / {num2} = {result}")
             print("Результат:", result)
     else:
         print("Неизвестная операция")
     continue_work = input("Продолжить работу? Да/Нет:")
     if continue_work == "Нет":
         print("Cпасибо за использование калькулятора!")
+        print("История операций:")
+        for entry in history:
+            print(entry)
     if continue_work != "Да" and continue_work != "Нет":
         print("Ошибка! Нужно вводить 'Да' или 'Нет'.")
         continue_work = input("Продолжить работу? Да/Нет:")
---
+++ list_test.py
@@ -0,0 +1,5 @@
+history = []
+history.append ("5 + 3 = 8")
+print(history)
+history.append ("10 - 3 = 7")
+print(history)
